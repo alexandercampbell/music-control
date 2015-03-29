2ca6b738439b9b0e69c6658b0bc5edecf62b7b49
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-03-29 12:44:15 -0500

    Finish prototyping a simple info server

---
+++ info.go
@@ -0,0 +1,23 @@
+package main
+
+import "os/exec"
+
+type Info struct {
+	Uname  string
+	Whoami string
+	PS     string
+}
+
+func get(command string, args ...string) string {
+	cmd := exec.Command(command, args...)
+	out, _ := cmd.Output()
+	return string(out)
+}
+
+var info = func() *Info {
+	return &Info{
+		Uname:  get("uname", "-a"),
+		Whoami: get("whoami"),
+		PS:     get("ps", "u"),
+	}
+}()
--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func launchMusic(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "GOOD MORNING")
+var (
+	indexTemplate, _ = template.ParseFiles("index.template")
+)
+
+func indexPage(w http.ResponseWriter, r *http.Request) {
+	indexTemplate.Execute(w, info)
 }
 
 func main() {
-	http.HandleFunc("/", launchMusic)
+	http.HandleFunc("/", indexPage)
+	fmt.Println("------------------------------------")
+	fmt.Println("Music Controller is running on :8000")
+	fmt.Println("------------------------------------")
+	fmt.Println(get("ifconfig"))
 	http.ListenAndServe(":8000", nil)
 }
