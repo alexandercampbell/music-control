84b52c903cb58a99cc2440a5ee5e12cace9ec5de
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-03-29 13:14:32 -0500

    Add non-functional endpoints to launch radio

--- main.go
+++ main.go
@@ -14,11 +14,25 @@ func indexPage(w http.ResponseWriter, r *http.Request) {
 	indexTemplate.Execute(w, info)
 }
 
+func startBBC(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Recieved request to launch BBC Radio from", r.RemoteAddr)
+}
+
+func startPolice(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Recieved request to launch police scanner from", r.RemoteAddr)
+}
+
 func main() {
 	http.HandleFunc("/", indexPage)
+	http.HandleFunc("/index.html", indexPage)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/start-bbc", startBBC)
+	http.HandleFunc("/start-police", startPolice)
+
+	fmt.Println(get("ifconfig"))
 	fmt.Println("------------------------------------")
 	fmt.Println("Music Controller is running on :8000")
 	fmt.Println("------------------------------------")
-	fmt.Println(get("ifconfig"))
+
 	http.ListenAndServe(":8000", nil)
 }
